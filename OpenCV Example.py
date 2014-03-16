feef612a23f5d6d64e4d0c9370753cd4bf557b22
AlexElson <alexmosenzov3@gmail.com>
2014-03-16 01:02:15 -0700

    Inverted Red

--- OpenCV Example.py
+++ OpenCV Example.py
@@ -9,7 +9,6 @@ if vc.isOpened(): # try to get the first frame
 else:
     rval = False
 
-
 def nothing(x):
     pass
 
@@ -19,11 +18,11 @@ cv2.namedWindow('image')
 
 # create trackbars for color change
 cv2.createTrackbar('H0','image',0,180,nothing)
-cv2.createTrackbar('S0','image',0,180,nothing)
-cv2.createTrackbar('V0','image',0,180,nothing)
+cv2.createTrackbar('S0','image',0,255,nothing)
+cv2.createTrackbar('V0','image',0,255,nothing)
 cv2.createTrackbar('H1','image',0,180,nothing)
-cv2.createTrackbar('S1','image',0,180,nothing)
-cv2.createTrackbar('V1','image',0,180,nothing)
+cv2.createTrackbar('S1','image',0,255,nothing)
+cv2.createTrackbar('V1','image',0,255,nothing)
 # create switch for ON/OFF functionality
 switch = '0 : OFF \n1 : ON'
 cv2.createTrackbar(switch, 'image',0,1,nothing)
@@ -35,9 +34,8 @@ cv2.setTrackbarPos('H1','image',100)
 cv2.setTrackbarPos('S1','image',120)
 cv2.setTrackbarPos('V1','image',60)
 
-
 while rval:
-    
+
     cv2.imshow('image',img)
 
     # get current positions of four trackbars
@@ -53,19 +51,21 @@ while rval:
 
     # Take each frame
     _, frame = vc.read()
+    invert=cv2.bitwise_not(frame,None)
 
     # Convert BGR to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(invert, cv2.COLOR_BGR2HSV)
     #hsv = cv2.dilate(hsv,None)
     #frame = cv2.GaussianBlur(frame,(5,5),0)
+    #hsv=cv2.bitwise_not(hsv,None)
 
     # # define range of blue color in HSV
-    # lower_blue = np.array([50,80,80])
+    # lower_blue = np.array([50,80,80]) 
     # upper_blue = np.array([100,200,200])
 
-    # define range of blue color in HSV
-    lower_blue = np.array([h0,s0,v0])
-    upper_blue = np.array([h1,s1,v1])
+    # define range of blue color in HSV #red
+    lower_blue = np.array([h0,s0,v0]) #72,96,141
+    upper_blue = np.array([h1,s1,v1]) #102,255,255
 
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
@@ -89,23 +89,14 @@ while rval:
         box = cv2.cv.BoxPoints(rect)
         box = np.int0(box)
         cv2.drawContours(frame,[box],0,(0,0,255),2)
-    
-    #minLineLength = 100
-    #maxLineGap = 10
-    #lines = cv2.HoughLinesP(gray,1,np.pi/180,100,minLineLength,maxLineGap)
-    #if lines is not None:
-    #    for x1,y1,x2,y2 in lines[0]:
-    #        cv2.line(frame,(x1,y1),(x2,y2),(50,255,50),2)
-        
-    #contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    #cv2.drawContours(gray, contours, -1, (100,100,100), 3)
 
     cv2.imshow('frame',frame)
-    cv2.imshow('gray',edges)
+    cv2.imshow('gray',gray)
 
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
         cv2.destroyWindow("frame")
         cv2.destroyWindow("gray")
+        cv2.destroyWindow("image")
         break
 
